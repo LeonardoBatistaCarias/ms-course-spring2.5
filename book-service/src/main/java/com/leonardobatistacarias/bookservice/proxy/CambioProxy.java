1adb0b06ab6402544136b043d95c97d4fef6f272
Leonardo Batista Carias <leonardo1carias@gmail.com>
2021-07-16 18:39:46 -0300

    Connecting the microsservices with Eureka Service Discovery

--- book-service/src/main/java/com/leonardobatistacarias/bookservice/proxy/CambioProxy.java
+++ book-service/src/main/java/com/leonardobatistacarias/bookservice/proxy/CambioProxy.java
@@ -5,8 +5,6 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-import java.math.BigDecimal;
-
 @FeignClient(name = "cambio-service", url = "http://localhost:8000/cambio-service")
 public interface CambioProxy {
 
--- cambio-service/src/main/java/com/leonardobatistacarias/cambioservice/CambioServiceApplication.java
+++ cambio-service/src/main/java/com/leonardobatistacarias/cambioservice/CambioServiceApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CambioServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CambioServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CambioServiceApplication.class, args);
+    }
 
 }
