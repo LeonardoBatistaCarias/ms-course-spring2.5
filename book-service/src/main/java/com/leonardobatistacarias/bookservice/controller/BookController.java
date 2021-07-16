5c661b07def7365d23ef686a8af72640765cf06e
Leonardo Batista Carias <leonardo1carias@gmail.com>
2021-07-16 18:24:45 -0300

    Examples of rest template and feign client synchron messages

--- book-service/src/main/java/com/leonardobatistacarias/bookservice/controller/BookController.java
+++ book-service/src/main/java/com/leonardobatistacarias/bookservice/controller/BookController.java
@@ -1,6 +1,7 @@
 package com.leonardobatistacarias.bookservice.controller;
 
 import com.leonardobatistacarias.bookservice.model.Book;
+import com.leonardobatistacarias.bookservice.proxy.CambioProxy;
 import com.leonardobatistacarias.bookservice.repository.BookRepository;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,7 +9,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping(value = "/book-service")
@@ -16,22 +18,49 @@ public class BookController {
 
     private final Environment environment;
     private final BookRepository bookRepository;
+    private final CambioProxy cambioProxy;
 
-    public BookController(Environment environment, BookRepository bookRepository) {
+    public BookController(Environment environment, BookRepository bookRepository, CambioProxy cambioProxy) {
         this.environment = environment;
         this.bookRepository = bookRepository;
+        this.cambioProxy = cambioProxy;
     }
 
     @GetMapping(value = "/{id}/{currency}")
     public Book findBook(@PathVariable("id") Long id, @PathVariable("currency") String currency) {
         var book = bookRepository.getById(id);
 
-        if(book == null) throw new RuntimeException("Book not found");
+        if (book == null) throw new RuntimeException("Book not found");
+
+        Map<String, String> params = new HashMap<>();
+        params.put("amount", book.getPrice().toString());
+        params.put("from", "USD");
+        params.put("to", currency);
+        var response = cambioProxy.getCambio(book.getPrice(), "USD", currency);
 
         var port = this.environment.getProperty("local.server.port");
         book.setEnvironment(port);
+        book.setPrice(response.getConvertedValue());
         return book;
     }
+
+//    @GetMapping(value = "/{id}/{currency}")
+//    public Book findBook(@PathVariable("id") Long id, @PathVariable("currency") String currency) {
+//        var book = bookRepository.getById(id);
+//
+//        if (book == null) throw new RuntimeException("Book not found");
+//
+//        Map<String, String> params = new HashMap<>();
+//        params.put("amount", book.getPrice().toString());
+//        params.put("from", "USD");
+//        params.put("to", currency);
+//        var response = new RestTemplate().getForEntity("http://localhost:8000/cambio-service/{amount}/{from}/{to}", Cambio.class, params);
+//
+//        var port = this.environment.getProperty("local.server.port");
+//        book.setEnvironment(port);
+//        book.setPrice(response.getBody().getConvertedValue());
+//        return book;
+//    }
 
 
 }
---
+++ book-service/src/main/java/com/leonardobatistacarias/bookservice/proxy/CambioProxy.java
@@ -0,0 +1,17 @@
+package com.leonardobatistacarias.bookservice.proxy;
+
+import com.leonardobatistacarias.bookservice.response.Cambio;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.math.BigDecimal;
+
+@FeignClient(name = "cambio-service", url = "http://localhost:8000/cambio-service")
+public interface CambioProxy {
+
+    @GetMapping(value = "/{amount}/{from}/{to}")
+    public Cambio getCambio(@PathVariable("amount") Double amount,
+                            @PathVariable("from") String from,
+                            @PathVariable("to") String to);
+}
